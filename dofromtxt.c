6f53d34b36dae81df9444cbb3f082a8f451e3d4a
Alexey Shoshin <36729934+AlexShoshin@users.noreply.github.com>
2018-10-09 10:16:22 +0300

    fixed mistakes

--- dofromtxt.c
+++ dofromtxt.c
@@ -17,19 +17,15 @@ struct Command
 	char** arguments;
 } typedef Command;
 
-// Может commandCount? Также вместо comarray можно написать commands просто.
-// Вы Split не используете явно в main. Можно убрать объявление совсем, а оставить только определение после main.
-
-void Split(char* string, char* delimiter, char** words, int* count);
-Command* ReadTxt(int* comcounter, int* timeout);
-void ExecuteCommands(Command* comarray, int comcounter, int timeout);
+Command* ReadTxt(int* commandCount, int* timeout);
+void ExecuteCommands(Command* commands, int commandCount, int timeout);
 
 int main()
 {
-	int comcounter = 0;
+	int commandCount = 0;
 	int timeout = 0;
-	Command* comarray = ReadTxt(&comcounter, &timeout);
-	ExecuteCommands(comarray, comcounter, timeout);
+	Command* commands = ReadTxt(&commandCount, &timeout);
+	ExecuteCommands(commands, commandCount, timeout);
 
 	return 0;
 }
@@ -49,10 +45,10 @@ void Split(char* string, char* delimiter, char** words, int* count)
 	free(tmp);
 }
 
-Command*  ReadTxt(int* comcounter, int* timeout)
+Command*  ReadTxt(int* commandCount, int* timeout)
 {
-	FILE* comfile = fopen("command.txt", "r");
-	if(comfile == NULL)
+	FILE* commandFile = fopen("command.txt", "r");
+	if(commandFile == NULL)
 	{
 		printf("Error with file\n");
 		return 0;
@@ -60,75 +56,80 @@ Command*  ReadTxt(int* comcounter, int* timeout)
 
 	char* str = (char*)malloc(STRING_LEN * sizeof(char));
 	int i, j;
-	int wordcounter;
+	int wordCounter;
 	char** words = (char**)malloc(ARG_QUANTITY * sizeof(char*));
 	for(i = 0; i < ARG_QUANTITY; i++)
 	{
 		words[i] = (char*)malloc(STRING_LEN * sizeof(char));
 	}
-	fgets(str, STRING_LEN, comfile);
-	// У вас эта ф-я вышла достаточно громоздкой. Лучше бы вынести её куски в отдельные ф-и, например,
-	// три строки ниже тянут на ParseHeaderLine
-	Split(str, " ", words, &wordcounter);
-	*comcounter = atoi(words[0]);
-	*timeout = atoi(words[1]);
-	Command* comarray = (Command*)malloc(*comcounter * sizeof(Command));
-	for(i = 0; i < *comcounter; i++)
+	fgets(str, STRING_LEN, commandFile);
+	ParseHeaderLine(str, words, wordCounter, commandCounter, timeout);
+	Command* commands = (Command*)malloc(*commandCount * sizeof(Command));
+	for(i = 0; i < *commandCount; i++)
 	{
-		fgets(str, STRING_LEN, comfile);
-		// А весь кусок ниже на ParseCommandLine
-		comarray[i].name = (char*)malloc(STRING_LEN * sizeof(char));
-		comarray[i].arguments = (char**)malloc(ARG_QUANTITY * sizeof(char*));
-		for(j = 0; j < ARG_QUANTITY; j++)
-		{
-			comarray[i].arguments[j] = (char*)malloc(STRING_LEN * sizeof(char));
-		}
-		Split(str, " ", comarray[i].arguments, &wordcounter);
-		comarray[i].arguments[wordcounter - 1][strlen(comarray[i].arguments[wordcounter - 1]) - 1] = '\0';
-		comarray[i].arguments[wordcounter] = NULL;
-		comarray[i].time = atoi(comarray[i].arguments[0]);
-		comarray[i].name = comarray[i].arguments[1];
+		fgets(str, STRING_LEN, commandFile);
+		ParseCommandLine(commands, str, &wordCounter);
 	}
-	fclose(comfile);
+	fclose(commandFile);
 	free(str);
 	for(i = 0; i < ARG_QUANTITY; i++)
 	{
 		free(words[i]);
 	}
 	free(words);
-	return comarray;
+	return commands;
 }
 
-// Мы договаривались про именовании переменных, что первое слово со строчной, остальные с заглавной: commandCount
-void ExecuteCommands(Command* comarray, int comcounter, int timeout)
+void ParseHeaderLine(char* str, char** words, int* wordCounter, int* commandCount, int* timeout)
 {
-	// FIXIT: pid1 и pid2 нужно переименовать в нечто более говорящее о предназначении переменной
-	int pid1, pid2;
+	Split(str, " ", words, wordCounter);
+	*commandCount = atoi(words[0]);
+	*timeout = atoi(words[1]);
+}
+
+void ParseCommandLine(Command* commands, char* str, int* wordCounter)
+{
+	commands[i].name = (char*)malloc(STRING_LEN * sizeof(char));
+	commands[i].arguments = (char**)malloc(ARG_QUANTITY * sizeof(char*));
+	for(j = 0; j < ARG_QUANTITY; j++)
+	{
+		commands[i].arguments[j] = (char*)malloc(STRING_LEN * sizeof(char));
+	}
+	Split(str, " ", commands[i].arguments, wordCounter);
+	commands[i].arguments[*wordCounter - 1][strlen(commands[i].arguments[*wordCounter - 1]) - 1] = '\0';
+	commands[i].arguments[*wordCounter] = NULL;
+	commands[i].time = atoi(commands[i].arguments[0]);
+	commands[i].name = commands[i].arguments[1];
+}
+
+void ExecuteCommands(Command* commands, int commandCount, int timeout)
+{
+	int externalProcessId, childProcessId;
 	int status;
 	int i;
 	int procid;
-	for(i = 0; i < comcounter; i++)
+	for(i = 0; i < commandCount; i++)
 	{
-		pid1 = fork();
-		if(pid1 == 0)
+		externalProcessId = fork();
+		if(externalProcessId == 0)
 		{
-			sleep(comarray[i].time);
-			pid2 = fork();
-			if(pid2 == 0)
+			sleep(commands[i].time);
+			childProcessId = fork();
+			if(childProcessId == 0)
 			{
-				execvp(comarray[i].name, (comarray[i].arguments + 1));
+				execvp(commands[i].name, (commands[i].arguments + 1));
 			}
 			else 
 			{
 				sleep(timeout);
-				if(waitpid(pid2, &status, WNOHANG))
+				if(waitpid(childProcessId, &status, WNOHANG))
 				{
 					exit(0);
 				}
 				else
 				{
-					kill(pid2, SIGKILL);
-					printf("Process %d killed\n", pid2);
+					kill(childProcessId, SIGKILL);
+					printf("Process %d killed\n", childProcessId);
 					exit(0);
 				}
 			}
